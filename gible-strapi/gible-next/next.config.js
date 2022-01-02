3093b94d7b16b1627b8172ab1bce06fc35c18451
TT1102 <t-trinh@ruby-dev.vn>
2022-01-02 17:36:20 +0700

    update layout

--- gible-strapi/gible-next/next.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	reactStrictMode: true,
-	env: {
-		JWT_SECRET: "kvbkgiughlsdjhljvbfjjiosdl",
-	},
-};
--- gible-strapi/gible-next/utils/baseUrl.js
+++
@@ -1,6 +0,0 @@
-const baseUrl =
-	process.env.NODE_ENV === "production"
-		? "https://gible-react.envytheme.com"
-		: "http://localhost:1337";
-
-export default baseUrl;
