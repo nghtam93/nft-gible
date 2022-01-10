33143dd3abe3617d5d98fd6c59576e6c8cd66034
TT1102 <t-trinh@ruby-dev.vn>
2022-01-10 08:43:40 +0700

    update header

--- components/Layout/Navbar.js
+++ components/Layout/Navbar.js
@@ -109,227 +109,38 @@ const Navbar = ({ wallet, openWallet, disconnectWallet }) => {
                         {/* <i className='ri-arrow-down-s-line'></i> */}
                       </a>
                     </Link>
-
-                    {/* <ul className='dropdown-menu'>
-                      <li className='nav-item'>
-                        <Link href='/' activeClassName='active'>
-                          <a className='nav-link active'>Home One</a>
-                        </Link>
-                      </li>
-                      <li className='nav-item'>
-                        <Link href='/index-2' activeClassName='active'>
-                          <a className='nav-link'>Home Two</a>
-                        </Link>
-                      </li>
-                      <li className='nav-item'>
-                        <Link href='/index-3' activeClassName='active'>
-                          <a className='nav-link'>Home Three</a>
-                        </Link>
-                      </li>
-                    </ul> */}
                   </li>
-
                   <li className="nav-item">
-                    <a href="#" className="nav-link ">
-                      Discover
-                      <i className="ri-arrow-down-s-line"></i>
-                    </a>
-                    <ul className="dropdown-menu">
-                      <li className="nav-item">
-                        <Link href="/auction" activeClassName="active">
-                          <a className="nav-link">Live Auction</a>
-                        </Link>
-                      </li>
-                      <li className="nav-item">
-                        <Link href="/discover" activeClassName="active">
-                          <a className="nav-link">Discover</a>
-                        </Link>
-                      </li>
-                      {/* <li className='nav-item'>
-                        <Link href='/discover-2' activeClassName='active'>
-                          <a className='nav-link'>Discover Style Two</a>
-                        </Link>
-                      </li> */}
-                      <li className="nav-item">
-                        <Link href="/item-details" activeClassName="active">
-                          <a className="nav-link">Item Details</a>
-                        </Link>
-                      </li>
-                    </ul>
+                    <Link href="/discover" activeClassName="active">
+                      <a className="nav-link">Discover</a>
+                    </Link>
                   </li>
-
                   <li className="nav-item">
                     <Link href="/activity" activeClassName="active">
                       <a className="nav-link">Activity</a>
                     </Link>
                   </li>
+
                   <li className="nav-item">
-                    <a href="#" className="nav-link">
-                      Pages
-                      <i className="ri-arrow-down-s-line"></i>
-                    </a>
-                    <ul className="dropdown-menu">
-                      <li className="nav-item">
-                        <Link href="/drops" activeClassName="active">
-                          <a className="nav-link">Drops</a>
-                        </Link>
-                      </li>
-
-                      <li className="nav-item">
-                        <Link href="/mint" activeClassName="active">
-                          <a className="nav-link">Mint</a>
-                        </Link>
-                      </li>
-
-                      <li className="nav-item">
-                        <Link href="/authors" activeClassName="active">
-                          <a className="nav-link">Authors</a>
-                        </Link>
-                      </li>
-
-                      <li className="nav-item">
-                        <Link href="/profile" activeClassName="active">
-                          <a className="nav-link">Profile</a>
-                        </Link>
-                      </li>
-
-                      {/* <li className='nav-item'>
-                        <Link href='/team' activeClassName='active'>
-                          <a className='nav-link'>Team</a>
-                        </Link>
-                      </li>
-
-                      <li className='nav-item'>
-                        <Link href='/testimonials' activeClassName='active'>
-                          <a className='nav-link'>Testimonials</a>
-                        </Link>
-                      </li> */}
-
-                      {/* <li className='nav-item'>
-                        <a href='#' className='nav-link'>
-                          User
-                          <i className='ri-arrow-down-s-line'></i>
-                        </a>
-                        <ul className='dropdown-menu'>
-                          <li className='nav-item'>
-                            <Link href='/login' activeClassName='active'>
-                              <a className='nav-link'>Log In</a>
-                            </Link>
-                          </li>
-                          <li className='nav-item'>
-                            <Link href='/register' activeClassName='active'>
-                              <a className='nav-link'>Register</a>
-                            </Link>
-                          </li>
-                          <li className='nav-item'>
-                            <Link
-                              href='/forgot-password'
-                              activeClassName='active'
-                            >
-                              <a className='nav-link'>Forgot Password</a>
-                            </Link>
-                          </li>
-                        </ul>
-                      </li> */}
-
-                      {/* <li className='nav-item'>
-                        <Link href='/terms-condition' activeClassName='active'>
-                          <a className='nav-link'>Terms & Conditions</a>
-                        </Link>
-                      </li>
-
-                      <li className='nav-item'>
-                        <Link href='/privacy-policy' activeClassName='active'>
-                          <a className='nav-link'>Privacy Policy</a>
-                        </Link>
-                      </li>
-
-                      <li className='nav-item'>
-                        <Link href='/404' activeClassName='active'>
-                          <a className='nav-link'>404 Page</a>
-                        </Link>
-                      </li>
-
-                      <li className='nav-item'>
-                        <Link href='/coming-soon' activeClassName='active'>
-                          <a className='nav-link'>Coming Soon</a>
-                        </Link>
-                      </li> */}
-                    </ul>
-                  </li>
-
-                  {/* <li className='nav-item'>
-                    <a href='#' className='nav-link'>
-                      Community
-                      <i className='ri-arrow-down-s-line'></i>
-                    </a>
-                    <ul className='dropdown-menu'>
-                      <li className='nav-item'>
-                        <Link href='/about' activeClassName='active'>
-                          <a className='nav-link'>About Us</a>
-                        </Link>
-                      </li>
-
-                      <li className='nav-item'>
-                        <Link href='/add-wallet' activeClassName='active'>
-                          <a className='nav-link'>Add Wallet</a>
-                        </Link>
-                      </li>
-
-                      <li className='nav-item'>
-                        <a href='#' className='nav-link'>
-                          Blog
-                          <i className='ri-arrow-down-s-line'></i>
-                        </a>
-                        <ul className='dropdown-menu'>
-                          <li className='nav-item'>
-                            <Link href='/blog-1' activeClassName='active'>
-                              <a className='nav-link'>Blog Grid</a>
-                            </Link>
-                          </li>
-                          <li className='nav-item'>
-                            <Link href='/blog-2' activeClassName='active'>
-                              <a className='nav-link'>Blog Left Sidebar</a>
-                            </Link>
-                          </li>
-                          <li className='nav-item'>
-                            <Link href='/blog-3' activeClassName='active'>
-                              <a className='nav-link'>Blog Right Sidebar</a>
-                            </Link>
-                          </li>
-                          <li className='nav-item'>
-                            <Link href='/blog-details' activeClassName='active'>
-                              <a className='nav-link'>Blog Details</a>
-                            </Link>
-                          </li>
-
-                          <li className='nav-item'>
-                            <Link href='/categories' activeClassName='active'>
-                              <a className='nav-link'>Categories</a>
-                            </Link>
-                          </li>
-
-                          <li className='nav-item'>
-                            <Link href='/tags' activeClassName='active'>
-                              <a className='nav-link'>Tags</a>
-                            </Link>
-                          </li>
-                        </ul>
-                      </li>
-
-                      <li className='nav-item'>
-                        <Link href='/help-center' activeClassName='active'>
-                          <a className='nav-link'>Help Center</a>
-                        </Link>
-                      </li>
-                    </ul>
-                  </li> */}
-
-                  {/* <li className='nav-item'>
-                    <Link href='/contact' activeClassName='active'>
-                      <a className='nav-link'>Contact Us</a>
+                    <Link href="/drops" activeClassName="active">
+                      <a className="nav-link">Drops</a>
                     </Link>
-                  </li> */}
+                  </li>
+                  <li className="nav-item">
+                    <Link href="/authors" activeClassName="active">
+                      <a className="nav-link">Authors</a>
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link href="/profile" activeClassName="active">
+                      <a className="nav-link">Authors Profile</a>
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link href="/mint" activeClassName="active">
+                      <a className="nav-link">Mint</a>
+                    </Link>
+                  </li>
                 </ul>
                 <div className="d-flex align-items-center">
                   <div className="others-options">
@@ -417,11 +228,22 @@ const Navbar = ({ wallet, openWallet, disconnectWallet }) => {
                           <a>Create</a>
                         </Link>
                       </li>
-                      <li>
-                        <Link href="/add-wallet" activeClassName="active">
-                          <a className="active">Connect Wallet</a>
-                        </Link>
-                      </li>
+                      {!wallet.is_connect && (
+                        <li>
+                          <a
+                            onClick={openWallet}
+                            className="active global-pointer">Connect Wallet
+                          </a>
+                        </li>
+                      )}
+                      {wallet.is_connect && (
+                        <li>
+                          <a
+                            onClick={handleDisconnectWallet}
+                            className="active global-pointer">Disconnect
+                          </a>
+                        </li>
+                      )}
                     </ul>
                   </div>
                 </div>
--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   reactStrictMode: true,
-  exportTrailingSlash: true,
+  trailingSlash: true,
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
